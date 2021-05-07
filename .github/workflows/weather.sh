7615693864ff08636597dbade878fc6ea7d97b4c
chenzhuonan <chenzhuonan@joyy.sg>
2021-05-07 11:38:49 +0800

    '修改action'

--- .github/workflows/weather.sh
+++ .github/workflows/weather.sh
@@ -1,12 +1,14 @@
-- name: 'Send mail'
-  uses: dawidd6/action-send-mail@master
-  with:
-    serveraddress: smtp.163.com
-    serverport: 465
-    username: ${{ secrets.MAILUSERNAME }}
-    password: ${{ secrets.MAILPASSWORD }}
-    subject: Shanghai Weather Report
-    body: file://result.html
-    to: yifeng.ruan@gmail.com
-    from: GitHub Actions
-    content_type: text/html
+#!/bin/sh
+
+set -eux
+
+CITY=Shanghai
+LANGUAGE="zh-CN"
+UNIT=m
+UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+
+curl \
+  -H "Accept-Language: $LANGUAGE" \
+  -H "User-Agent: $UA" \
+  -o result.html \
+  wttr.in/$CITY?format=4\&$UNIT
